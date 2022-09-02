10bc5f6f9efc5845ac80c40fe864a8b9ef9b2e91
debtil <75885796+debtil@users.noreply.github.com>
2022-09-02 14:05:42 -0300

    mudança de pasta

--- IONIC/TrabalhoIONIC/AppMusicas/src/app/app-routing.module.ts
+++ IONIC/TrabalhoIONIC/AppMusicas/src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./pages/cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'detalhes',
+    loadChildren: () => import('./pages/detalhes/detalhes.module').then( m => m.DetalhesPageModule)
   },
 ];
 
