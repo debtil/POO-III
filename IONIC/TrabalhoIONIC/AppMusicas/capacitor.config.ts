e769448c27d5aa6ffc72ffd44583d4a393856dd1
debtil <75885796+debtil@users.noreply.github.com>
2022-09-02 13:42:34 -0300

    teste1

---
+++ IONIC/TrabalhoIONIC/AppMusicas/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppMusicas',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
