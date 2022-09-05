ae271072f092037db37c361d2fc50144cbb0247c
debtil <75885796+debtil@users.noreply.github.com>
2022-09-05 09:34:26 -0300

    triste

--- IONIC/TrabalhoIONIC/Musicafy/Musicafy/src/app/app-routing.module.ts
+++ IONIC/TrabalhoIONIC/Musicafy/Musicafy/src/app/app-routing.module.ts
@@ -10,6 +10,14 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'cadastrar',
+    loadChildren: () => import('./pages/cadastrar/cadastrar.module').then( m => m.CadastrarPageModule)
+  },
+  {
+    path: 'detalhar',
+    loadChildren: () => import('./pages/detalhar/detalhar.module').then( m => m.DetalharPageModule)
   }
 ];
 
